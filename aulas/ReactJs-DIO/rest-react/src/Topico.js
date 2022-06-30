7f1b51f7f4426de1f65c1b01f9863fc1ee95dd7b
thisd92 <thiiagodutra@gmail.com>
2022-06-30 16:50:33 -0300

    Fetch/Axios, Imutabilidade, Redux e Rest

---
+++ aulas/ReactJs-DIO/rest-react/src/Topico.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components'
+
+const AppBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Item = styled.li`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 12px;
+  border: 1px solid #cecece;
+`;
+
+function App() {
+  const [cientistas, setCientistas] = useState([]);
+  
+  // Hook equivalente ao componentDidMount()
+  useEffect(() => {
+    fetch('https://react-intermediario-dio.free.beeceptor.com/cientistas-brasileiras')
+      .then(response => response.json())
+      .then(data => {
+      setCientistas(data)
+      })
+      .catch(error => {
+        alert('Erro: ' + error);
+      })
+  }, []);
+  
+  return (
+    <AppBlock>
+      <h1>Cientistas Brasileiras</h1>
+      <ul>
+        {cientistas.map((cientista, index) => (
+          <Item key={index}>
+            <div><b>Nome:</b> {cientista.name}</div>
+            <div><b>Área:</b> {cientista.area}</div>
+          </Item>
+        ))}
+      </ul>
+    </AppBlock>
+  );
+}
+
+export default Topico;
