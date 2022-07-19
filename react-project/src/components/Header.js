6fdf51e128902257f920fcf41341055e03123ae0
thisd92 <thiiagodutra@gmail.com>
2022-07-19 17:11:24 -0300

    add styled, header and layout

---
+++ react-project/src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import * as S from "./styled";
+import useGithub from "../hooks/github-hooks";
+
+const Header = () => {
+  const { getUser } = useGithub();
+  const [usernameForSearch, setUsernameForSearch] = useState();
+
+  const submitGetUser = () => {
+    if (!usernameForSearch) return;
+    return getUser(usernameForSearch);
+  };
+
+  return (
+    <header>
+      <S.Wrapper2>
+        <input
+          type="text"
+          placeholder="Digite o username para pesquisa..."
+          onChange={(event) => setUsernameForSearch(event.target.value)}
+        />
+        <button type="submit" onClick={submitGetUser}>
+          <span>Buscar</span>
+        </button>
+      </S.Wrapper2>
+    </header>
+  );
+};
+
+export default Header;
--- react-project/src/components/Layout.js
+++ react-project/src/components/Layout.js
@@ -1,3 +1,12 @@
-export const Layout = () => {
-    
-}
+import React from "react";
+import Header from "./Header";
+import * as S from "./styled";
+
+export const Layout = ({ children }) => {
+  return (
+    <S.WrapperLayout>
+      <Header />
+      {children}
+    </S.WrapperLayout>
+  );
+};
